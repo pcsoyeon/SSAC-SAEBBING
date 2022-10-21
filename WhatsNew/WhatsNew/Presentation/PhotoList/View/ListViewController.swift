b591fddb68b0364b2cd506fd2d1d6e37a5459bce
so yeon <alicesysy@swu.ac.kr>
2022-10-21 17:09:49 +0900

    [#5] 프로젝트 추가 및 폴더링

---
+++ WhatsNew/WhatsNew/Presentation/PhotoList/View/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsNew
+//
+//  Created by 소연 on 2022/10/21.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
