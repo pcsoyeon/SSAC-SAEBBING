13972c7900bbc795ae6fbd9391b3e5614dfebf94
so yeon <alicesysy@swu.ac.kr>
2022-10-25 17:21:46 +0900

    [#9] Rx로 SearchBar 이벤트 처리

--- WhatsNew/WhatsNew/Presentation/SearchPhoto/View/SearchPhotoViewController.swift
+++ WhatsNew/WhatsNew/Presentation/SearchPhoto/View/SearchPhotoViewController.swift
@@ -30,24 +30,47 @@ final class SearchPhotoViewController: UIViewController {
         super.viewDidLoad()
         configureUI()
         setLayout()
-        
-        viewModel.requestPhotoList("apple")
-        viewModel.list.bind { photoList in
-            dump(photoList)
-        }
+        bindData()
     }
     
     // MARK: - UI Method
     
     private func configureUI() {
+        view.backgroundColor = .white
+        
         configureCollectionView()
+        configureSearchBar()
     }
     
     private func setLayout() {
+        view.addSubview(searchBar)
         
+        searchBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(44)
+        }
     }
     
     private func configureCollectionView() {
         
+    }
+    
+    private func configureSearchBar() {
+        searchBar.rx.text.orEmpty
+            .debounce(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .subscribe(with: self) { (vc, value) in
+                self.viewModel.requestPhotoList(value)
+                print(value)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Data
+    
+    private func bindData() {
+        viewModel.list.bind { [weak self] photoList in
+            guard let self = self else { return }
+            dump(photoList)
+        }
     }
 }
