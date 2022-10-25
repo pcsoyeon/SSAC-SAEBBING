7af3df60062df678170939ddd345629fe9e8fab3
so yeon <alicesysy@swu.ac.kr>
2022-10-25 17:30:17 +0900

    [#9] DiffableDataSource, CompositionalLayout으로 UI 구성

--- WhatsNew/WhatsNew/Presentation/SearchPhoto/View/SearchPhotoViewController.swift
+++ WhatsNew/WhatsNew/Presentation/SearchPhoto/View/SearchPhotoViewController.swift
@@ -18,7 +18,11 @@ final class SearchPhotoViewController: UIViewController {
     
     private var searchBar = UISearchBar()
     
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    
     // MARK: - Propery
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Int, Photo>!
     
     private let viewModel = SearchPhotoViewModel()
     
@@ -38,21 +42,34 @@ final class SearchPhotoViewController: UIViewController {
     private func configureUI() {
         view.backgroundColor = .white
         
-        configureCollectionView()
+        configureDataSource()
         configureSearchBar()
     }
     
     private func setLayout() {
         view.addSubview(searchBar)
+        view.addSubview(collectionView)
         
         searchBar.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(44)
         }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
-    private func configureCollectionView() {
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<ListCollectionViewCell, Photo>.init { cell, indexPath, itemIdentifier in
+            cell.setData(itemIdentifier)
+        }
         
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
     }
     
     private func configureSearchBar() {
@@ -70,7 +87,38 @@ final class SearchPhotoViewController: UIViewController {
     private func bindData() {
         viewModel.list.bind { [weak self] photoList in
             guard let self = self else { return }
-            dump(photoList)
+            
+            var snapshot = NSDiffableDataSourceSnapshot<Int, Photo>()
+            snapshot.appendSections([0])
+            snapshot.appendItems(photoList)
+            
+            self.dataSource.apply(snapshot)
+        }
+    }
+}
+
+// MARK: - CollectionView
+
+extension SearchPhotoViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        let layout = createCompositionalLayout()
+        layout.configuration = configuration
+        return layout
+    }
+    
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionIndex, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                  heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .absolute(140))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            return section
         }
     }
 }
