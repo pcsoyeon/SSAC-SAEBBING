fe23ae85f23c84053e6d8fb47eb70dc7f99c9821
so yeon <alicesysy@swu.ac.kr>
2022-11-02 07:14:18 +0900

    [#15] URLSession 프로젝트 추가

---
+++ URLSession/URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSession
+//
+//  Created by 소연 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
